c61a734d9c7604efda6ed5b9498e14cbc5021e1a
aaron99 <aaron99@vt.edu>
2023-12-31 12:06:57 -0500

    first commit

---
+++ NeuralNets.py
@@ -0,0 +1,294 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+
+class NN(object):
+    def __init__(self,name = "Neural Net"):
+        self.name = name
+
+class FullyConnected(NN):
+    def __init__(self,inputDim,outputDim,NPL,HiddenDim,minibatch_sz, activation_fcn="ReLU",cost_func="MSE", optimizer="SGD"):
+        super().__init__()
+        self.inputDim = inputDim
+        self.outputDim = outputDim
+        self.NPL = NPL # Neurons per layer
+        self.HiddenDim = HiddenDim
+        self.minibatch_sz = minibatch_sz
+        self.activation_fcn=activation_fcn
+        self.optimizer = optimizer
+
+        self.parameters = dict()
+        self.gradients = dict()
+        self.init_weights()
+        self.init_gradients()
+        self.init_intermediate_vals()
+        self.init_activationFunc(activation_fcn)
+        self.init_alg_params()
+        
+        self.costFunc = self.MSE
+        self.mse_history = []
+
+    def init_gradients(self):
+        self.gradients = {
+            "weights_in": np.zeros((self.inputDim, self.NPL)),
+            "weights": np.zeros((self.NPL, self.NPL, self.HiddenDim)),
+            "weights_out": np.zeros((self.NPL, self.outputDim)),
+            "bias": np.zeros((self.NPL, self.HiddenDim)),
+            "output_bias": np.zeros((self.outputDim, 1)).transpose(),
+            "input_bias": np.zeros((self.NPL, 1))
+        }
+        self.gradients_sample = {
+            "weights_in": np.zeros((self.inputDim, self.NPL)),
+            "weights": np.zeros((self.NPL, self.NPL, self.HiddenDim)),
+            "weights_out": np.zeros((self.NPL, self.outputDim)),
+            "bias": np.zeros((self.NPL, self.HiddenDim)),
+            "output_bias": np.zeros((self.outputDim, 1)).transpose(),
+            "input_bias": np.zeros((self.NPL, 1))
+        }
+
+
+    def init_weights(self):
+        self.parameters = {
+            "weights_in": (np.random.randn(self.inputDim, self.NPL) * np.sqrt(1.0 / self.inputDim)).T,
+            "weights": np.random.randn(self.NPL, self.NPL, self.HiddenDim) * np.sqrt(2.0 / self.NPL),
+            "weights_out": (np.random.randn(self.NPL, self.outputDim) * np.sqrt(1.0 / self.NPL)).T,
+            "bias": np.zeros((self.NPL, self.HiddenDim)),
+            "output_bias": np.zeros((self.outputDim, 1)),
+            "input_bias": np.zeros((self.NPL, 1))
+        }
+
+    def init_intermediate_vals(self):
+        self.a = np.zeros((self.NPL, self.HiddenDim +1  ,self.minibatch_sz))
+        self.z = np.zeros((self.NPL, self.HiddenDim +1,self.minibatch_sz))
+    def init_alg_params(self):
+        if self.optimizer == "ADAM":
+             self.adam_m = {param_name: np.zeros_like(param_value) for param_name, param_value in self.parameters.items()}
+             self.adam_v = {param_name: np.zeros_like(param_value) for param_name, param_value in self.parameters.items()}
+        
+            
+    def init_activationFunc(self,activation_fcn):
+        if activation_fcn == "ReLU":
+            self.activationFunc = self.ReLU
+        elif activation_fcn == "Tanh":
+            self.activationFunc = self.tanh
+        elif activation_fcn == "Sigmoid":
+            self.activationFunc = self.sigmoid
+        elif activation_fcn == "SiLU":
+            self.activationFunc = self.silu
+        else:
+            raise RuntimeError("Unspecified activation function")
+    
+    def forward_sample(self,x_initial,sample_no): 
+            self.z[:,0,sample_no] = self.parameters["weights_in"] @ x_initial + self.parameters["input_bias"].flatten()
+            self.a[:,0,sample_no] = self.parameters["weights_in"] @ x_initial + self.parameters["input_bias"].flatten()
+
+            for i in range(self.HiddenDim):
+                self.z[:,i+1,sample_no] = self.parameters["weights"][:, :, i] @ self.a[:, i,sample_no] +self.parameters["bias"][:, i-1]
+                self.a[:,i+1,sample_no] = self.activationFunc(self.z[:,i,sample_no],derivative=False)
+
+            return self.parameters["weights_out"] @ self.a[:,self.HiddenDim,sample_no] +self.parameters["output_bias"]
+    
+
+    def forward(self,x_minibatch):
+        
+        y_out = np.zeros((self.minibatch_sz,self.outputDim))
+        for i,x in enumerate(x_minibatch):
+            y_out[i,:] = self.forward_sample(x,sample_no=i)
+        return y_out
+    
+
+
+    def clip_gradients(self, threshold):
+        pass
+        
+    def zero_gradient(self):
+        for key in self.gradients.keys():
+            value = self.gradients[key]
+            self.gradients[key] = np.zeros_like(value)
+    def zero_grad_samples(self):
+        for key in self.gradients_sample.keys():
+            value = self.gradients_sample[key]
+            self.gradients_sample[key] = np.zeros_like(value)
+        
+    def gradient(self,ypred,yactual,xactual):
+
+        def grad_sample(self,ypred_sample,yactual_sample,sample_no,xinput_sample):
+            output_error  = self.costFunc(ypred_sample,yactual_sample,derivative=True)
+            delta_prev = (np.ones((1,self.outputDim))*output_error).T
+            self.gradients_sample["weights_out"] = self.a[:,-1,sample_no].reshape(self.NPL,1) @ delta_prev
+            self.gradients_sample["output_bias"] = delta_prev
+
+            if self.HiddenDim >= 1:
+                
+                delta = (self.parameters["weights_out"].T @  delta_prev)*self.activationFunc(self.z[:,self.HiddenDim-1,sample_no],derivative=True).transpose().reshape(self.NPL,1)
+                self.gradients_sample["weights"][:,:,self.HiddenDim-1] = self.a[:,-2,sample_no].reshape(self.NPL,1).transpose() @ delta
+                self.gradients_sample["bias"][:,-1] = delta.flatten()
+                delta_prev = delta
+            ## Hidden gradients
+            if self.HiddenDim > 1:
+                for i in range(self.HiddenDim -2, 0, -1):
+                    delta = (self.parameters["weights"][:,:,i].T @  delta_prev)*self.activationFunc(self.z[:,i,sample_no],derivative=True).transpose().reshape(self.NPL,1)
+                    self.gradients_sample["weights"][:,:,i] = self.a[:,i,sample_no].reshape(self.NPL,1).transpose() @ delta
+                    self.grad_bias_sample[:,i] = delta.flatten()
+                    delta_prev = delta
+            # input layers
+            
+            if self.HiddenDim==0:
+                delta = (self.parameters["weights_out"].T @  delta_prev).T
+                self.gradients_sample["weights_in"] = xinput_sample.reshape((len(xinput_sample),1)) @ delta
+                self.gradients_sample["input_bias"] = delta
+            else:
+                delta = (self.parameters["weights"][:,:,0].T @  delta_prev).T * self.activationFunc(self.z[:,0,sample_no],derivative=True)
+                self.gradients_sample["weights_in"] = xinput_sample.reshape((len(xinput_sample),1)) @ delta
+                self.gradients_sample["input_bias"] = delta
+
+            
+
+            
+        for sample_num in range(len(yactual)):
+            grad_sample(self,ypred[sample_num],yactual[sample_num],sample_num,xactual[sample_num])
+            self.updateSampleWeights()
+        
+        self.zero_grad_samples()
+    def updateSampleWeights(self):
+        for key in self.gradients_sample.keys():
+            gradient_sample = self.gradients_sample[key]
+            if key=="output_bias" or key=="input_bias":
+                gradient_sample = gradient_sample.T
+            self.gradients[key] += gradient_sample/self.minibatch_sz
+
+
+        
+        
+        
+
+    def updateWeights(self,lr = .001,alg = "SGD",beta1 = .9,beta2 = .999,epsilon=1e-8):
+        if alg == "SGD":
+            self.SGD(lr = lr)
+        if alg == "ADAM":
+            self.ADAM(lr = .001,beta1 = .9,beta2 = .999,epsilon=1e-8)
+        self.zero_gradient()
+
+
+       
+################################################################
+#               OPTIMIZER FUNCTIONS
+################################################################
+    
+    def SGD(self,lr=.0001):
+        for grad_name, grad_value in self.gradients.items():
+            if grad_name == "weights_out" or grad_name == "weights_in":
+                grad_value = grad_value.T
+            self.parameters[grad_name] -= lr* grad_value
+    def ADAM(self, lr=0.001, beta1=0.9, beta2=0.999, epsilon=1e-8):
+        for param_name, param_value in self.parameters.items():
+            # Retrieve ADAM variables
+            m = self.adam_m[param_name]
+            v = self.adam_v[param_name]
+
+            # Compute gradients (assuming you have a gradients dictionary)
+            gradient = self.gradients.get(param_name, np.zeros_like(param_value))
+            if param_name=="weights_out" or param_name=="weights_in":
+                gradient = gradient.T
+
+            # Update moments
+            m = beta1 * m + (1 - beta1) * gradient
+            v = beta2 * v + (1 - beta2) * (gradient ** 2)
+
+            # Bias-corrected moments
+            m_hat = m / (1 - beta1)
+            v_hat = v / (1 - beta2)
+
+            # Update parameters
+            self.parameters[param_name] -= lr * m_hat / (np.sqrt(v_hat) + epsilon)
+
+            # Update ADAM variables
+            self.adam_m[param_name] = m
+            self.adam_v[param_name] = v
+
+
+
+
+
+##################################################################
+                # LOSS FUNCTIONS
+##################################################################
+    @staticmethod
+    def MSE(ypred,yactual,derivative = False):
+        if not derivative:
+            return np.mean((ypred-yactual)**2)
+        return 2*(ypred-yactual)/len(ypred)
+    @staticmethod
+    def cross_entropy_loss(y_pred, y_actual, epsilon=1e-15, derivative=False):
+
+        y_pred = np.clip(y_pred, epsilon, 1 - epsilon)
+
+        # Calculate cross-entropy loss
+        loss = -np.sum(y_actual * np.log(y_pred)) / len(y_actual)
+
+        if not derivative:
+            return loss
+
+        # Calculate the gradient of the cross-entropy loss
+        gradient = (y_pred - y_actual) / (y_pred * (1 - y_pred) * len(y_actual))
+
+        return gradient
+
+
+####################################################################
+                # ACTIVATION FUNCTIONS
+###################################################################
+    @staticmethod
+    def ReLU(x,derivative = False):
+        if not derivative:
+            return np.maximum(0, x)
+        return np.where(x > 0, 1, 0)
+
+
+    @staticmethod
+    def tanh(x, derivative=False):
+        if not derivative:
+            return np.tanh(x)
+        return 1 - np.tanh(x)**2
+
+    @staticmethod
+    def sigmoid(x, derivative=False):
+        if not derivative:
+            return 1 / (1 + np.exp(-x))
+        return x * (1 - x)
+    
+    @staticmethod
+    def silu(x, derivative=False):
+        sigmoid_x = 1 / (1 + np.exp(-x))
+
+        if not derivative:
+            return x * sigmoid_x
+        return sigmoid_x * (1 + x * (1 - sigmoid_x))
+
+
+
+
+
+
+        
+
+           
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ pytorch_NN.py
@@ -0,0 +1,87 @@
+
+## Aaron Pueschel
+## Final Project Optimization
+#############################################
+
+## Imports: make sure you have Torch, numpy, matplotlib
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import matplotlib.pyplot as plt
+import torch
+import numpy as np
+from torch.utils.data import Dataset, DataLoader
+from random import randrange
+import matplotlib.pyplot as plt
+from torch.utils.data import Dataset, DataLoader, random_split
+
+
+## DEFINE CLASSES
+class NeuralNetwork(nn.Module):
+    def __init__(self, input_dim, hidden_dim, output_dim):
+        super(NeuralNetwork, self).__init__()
+        self.layer1 = nn.Linear(input_dim, hidden_dim,bias=True)
+        self.act1 = nn.ReLU()
+        #self.layer2 = nn.ReLU()
+        #self.act2 = nn.ReLU()
+        self.out_layer = nn.Linear(hidden_dim,output_dim,bias=False)
+        
+
+    def forward(self, x):
+        x = self.layer1(x)
+        x = self.act1(x) 
+        #x = self.layer2(x) 
+        #x = self.act2(x) 
+        x = self.out_layer(x) 
+        return x
+
+## Create a dataloader class for minibatching
+class CustomDataset(Dataset):
+    def __init__(self, data, labels):
+        self.data = data
+        self.labels = labels
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        return self.data[idx], self.labels[idx]
+    
+############################################
+    
+    # FUNCTIONS   
+
+#############################################
+
+class functions():
+    def generate_data(xrange,yrange,num_vals):
+        x1 = np.linspace(xrange[0],xrange[1],num_vals)
+        x2 = np.linspace(yrange[0],yrange[1],num_vals)
+        np.random.shuffle(x1)
+        np.random.shuffle(x2)
+        return x1,x2
+        
+    def himmelblau_noisy(x1_train, x2_train, noise_level=0.001):
+        true_value = (x1_train**2 + x2_train - 11)**2 + (x1_train + x2_train**2 - 7)**2
+        noise = np.random.normal(0, noise_level, true_value.shape)
+        return true_value + noise
+
+    def rosenbrock_noisy(x1, x2, noise_level=0.001):
+        a = 1
+        b = 3
+        true_value = (a * (x2 - x1**2)**2) + (b * (1 - x1)**2)
+        noise = np.random.normal(0, noise_level, true_value.shape)
+        return true_value + noise
+    def ackley(x1, x2, noise_level=0.001):
+        a = 20
+        b = 0.2
+        c = 2 * np.pi
+        sum1 = x1**2 + x2**2
+        sum2 = np.cos(c * x1) + np.cos(c * x2)
+        term1 = -a * np.exp(-b * np.sqrt(sum1 / 2))
+        term2 = -np.exp(sum2 / 2)
+        result = term1 + term2 + a + np.exp(1)
+        noise = np.random.normal(0, noise_level)
+        return result + noise
+
+
